50ebab30f6d86776fc7025c77f82ccd166e34cd0
Sulakshiilangesh <106367870+Sulakshiilangesh@users.noreply.github.com>
2022-08-24 17:42:15 +0530

    Add files via upload

---
+++ traWELL/scripts/main.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../../serviceWorker.js')
+    .then((reg) => {
+        console.log("Service Worker registered.", reg);
+    })
+    .catch((err) => {
+        console.log("Service Worker not registered.", err);
+    })
+
+}
